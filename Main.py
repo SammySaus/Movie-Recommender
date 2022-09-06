901384e52bc59a1f470ad050cd82ccec3be306c1
Sam <Samuel.hoyle@gmail.com>
2022-09-06 17:03:03 +0000

    created loads of new functions. slowly working through tutorial

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from ast import literal_eval
+import numpy as np
 
 metadata = pd.read_csv('archive/movies_metadata.csv', low_memory=False)
 metadata.head(3)  # tell pandas how much is a header
@@ -52,3 +54,90 @@ print(tfidf.get_feature_names_out()[5000:5010])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 print(f"Cosine_sim Shape: {cosine_sim.shape}")
 print(cosine_sim[1])
+indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    # get the index of the movie that matches the title
+    idx = indices[title]
+
+    # get the pairwise similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # sort the movies based on similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # return the top 10 most similar titles
+    return metadata['title'].iloc[movie_indices]
+
+
+# Load keywords and credits
+credits = pd.read_csv('archive/credits.csv')
+keywords = pd.read_csv('archive/keywords.csv')
+
+# Remove rows with bad IDs.
+metadata = metadata.drop([19730, 29503, 35587])
+
+# Convert IDs to int. Required for merging
+keywords['id'] = keywords['id'].astype('int')
+credits['id'] = credits['id'].astype('int')
+metadata['id'] = metadata['id'].astype('int')
+
+# Merge keywords and credits into your main metadata dataframe
+metadata = metadata.merge(credits, on='id')
+metadata = metadata.merge(keywords, on='id')
+
+features = ['cast', 'crew', 'keywords', 'genres']
+for feature in features:
+    metadata[feature] = metadata[feature].apply(literal_eval)
+
+
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+
+
+def get_list(x):
+    if isinstance(x, list):
+        names = [i['name'] for i in x]
+        if len(names) > 3:
+            names = names[:3]
+        return names
+    return []
+
+
+metadata['director'] = metadata['crew'].apply(get_director)
+
+features = ['cast', 'keywords', 'genres']
+for feature in features:
+    metadata[feature] = metadata[feature].apply(get_list)
+
+# Print the new features of the first 3 films
+metadata[['title', 'cast', 'director', 'keywords', 'genres']].head(3)
+
+
+# Function to convert all strings to lower case and strip names of spaces
+def clean_data(x):
+    if isinstance(x, list):
+        return [str.lower(i.replace(" ", "")) for i in x]
+    else:
+        # Check if director exists. If not, return empty string
+        if isinstance(x, str):
+            return str.lower(x.replace(" ", ""))
+        else:
+            return ''
+
+
+# Apply clean_data function to your features.
+features = ['cast', 'keywords', 'director', 'genres']
+
+for feature in features:
+    metadata[feature] = metadata[feature].apply(clean_data)
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Movie-Recommender
 Movie Recommendation using Python
 dataset from https://www.kaggle.com/datasets/rounakbanik/the-movies-dataset?resource=download
+
+tutorial from
+https://www.datacamp.com/tutorial/recommender-systems-python
